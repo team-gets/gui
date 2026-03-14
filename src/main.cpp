d0479c4b355af8ef6b8ac3fcb1540aaa614d90d9
JC Luna <jc.lunaa@protonmail.com>
2026-03-13 21:25:45 -0500

    Re-Add load from source from cli

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@ int main(int argc, char** argv) {
 	QApplication app(argc, argv);
 
 	VSCL::DevWindow window;
+	if (argc > 1) { window.SetQMLFromPath(QUrl::fromLocalFile(argv[1])); }
+
 	window.show();
 	return app.exec();
 }

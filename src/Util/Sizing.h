a940386102945ab54de9cd90d0a381bfadff089b
JC Luna <jc.lunaa@protonmail.com>
2026-03-16 17:45:51 -0500

    A font sizer

---
+++ src/Util/Sizing.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <QWidget>
+
+namespace VSCL::Util {
+
+constexpr int MinimumWidth = 480;
+constexpr int MinimumHeight = 720;
+
+struct FontAdjustment {
+	int PointSizeAtMinimum;
+	bool AdjustToWidth = true;
+	const int AdjustPointSize(QWidget* parent) const {
+		return PointSizeAtMinimum
+			+ ((AdjustToWidth) ? parent->width() - MinimumWidth : parent->height() - MinimumHeight);
+	}
+};
+} // namespace VSCL::Util

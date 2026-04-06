44879aef1e1dcdc19a3f0e40c5775b3fc30738b1
JC Luna <jc.lunaa@protonmail.com>
2026-04-05 20:59:20 -0500

    Don't use generator expressions in add_test

--- cmake/cpp.cmake
+++ cmake/cpp.cmake
@@ -77,9 +77,9 @@ endfunction(add_subdirectory_silence_warnings IN_DIRECTORY)
 
 function(target_copy_dll IN_TARGET)
 	if (WIN32)
-		add_custom_command(TARGET IN_TARGET POST_BUILD
+		add_custom_command(TARGET ${IN_TARGET} POST_BUILD
 		COMMAND ${CMAKE_COMMAND} -E copy_if_different
-			$<TARGET_RUNTIME_DLLS:IN_TARGET> $<TARGET_FILE_DIR:IN_TARGET>
+			$<TARGET_RUNTIME_DLLS:${IN_TARGET}> $<TARGET_FILE_DIR:${IN_TARGET}>
 		COMMAND_EXPAND_LISTS)
 	endif()
 endfunction(target_copy_dll_to_other IN_TARGET IN_OTHER)

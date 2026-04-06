c3d3cae74396b2183b1f0673d0a186efc74f516a
JC Luna <jc.lunaa@protonmail.com>
2026-04-05 20:55:23 -0500

    Copy DLLs for testing

--- cmake/cpp.cmake
+++ cmake/cpp.cmake
@@ -74,3 +74,12 @@ function(add_subdirectory_silence_warnings IN_DIRECTORY)
 
 	set_directory_properties(PROPERTIES COMPILE_OPTIONS "${oldCompileOpts}")
 endfunction(add_subdirectory_silence_warnings IN_DIRECTORY)
+
+function(target_copy_dll IN_TARGET)
+	if (WIN32)
+		add_custom_command(TARGET IN_TARGET POST_BUILD
+		COMMAND ${CMAKE_COMMAND} -E copy_if_different
+			$<TARGET_RUNTIME_DLLS:IN_TARGET> $<TARGET_FILE_DIR:IN_TARGET>
+		COMMAND_EXPAND_LISTS)
+	endif()
+endfunction(target_copy_dll_to_other IN_TARGET IN_OTHER)
